b1d5ee27ab96fa4c4db7d5750114a36a672cba43
sol <solrasido55@gmail.com>
2022-10-19 10:05:53 +0900

    Modify:테스트용 코드 삭제

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,12 +2,7 @@ import React from "react";
 import Nav from "../../components/Nav/Nav";
 
 function Main() {
-  return (
-    <div className="main">
-      <Nav />
-      <div>위코드 38기 3팀 weKEA입니다.</div>
-    </div>
-  );
+  return <div>위코드 38기 3팀 weKEA입니다.</div>;
 }
 
 export default Main;
