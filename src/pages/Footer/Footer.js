ddd6507dbeeefeca66b9e635900bb430b6722d95
김도영 <go3152@gmail.com>
2022-10-19 16:22:53 +0900

    ADD:메인 푸터 폴더 및 파일 생성

---
+++ src/pages/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Footer() {
+  return <h2>ggdgd</h2>;
+}
+export default Footer;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Footer from "../Footer/Footer";
 function Main() {
-  return <div>위코드 38기 3팀 weKEA입니다.</div>;
+  return (
+    <div>
+      <Footer />
+      위코드 38기 3팀 weKEA입니다.
+    </div>
+  );
 }
 
 export default Main;
