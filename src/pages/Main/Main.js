0071e3fb6e4e9ab4f361b71ea79e9df477e8ec10
김도영 <go3152@gmail.com>
2022-10-20 14:20:49 +0900

    ADD: 리스트페이지 브랜치/폴더/파일

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Detail from "../Detail/Detail";
 function Main() {
-  return <div>위코드 38기 3팀 weKEA입니다.</div>;
+  return (
+    <div>
+      <div>
+        <Detail />
+      </div>
+      위코드 38기 3팀 weKEA입니다.
+    </div>
+  );
 }
 
 export default Main;
