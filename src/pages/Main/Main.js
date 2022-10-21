de28cf0989633cb7a7b102481ecc69d6f004397d
김도영 <go3152@gmail.com>
2022-10-21 16:28:10 +0900

    modify:리스트페이지 확인차 메인페이지 사용했던 것 원상복구

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,15 +1,7 @@
 import React from "react";
-import Listpage from "../Listpage/Listpage";
 
 function Main() {
-  return (
-    <div>
-      <div>
-        <Listpage />
-      </div>
-      위코드 38기 3팀 weKEA입니다.
-    </div>
-  );
+  return <div>위코드 38기 3팀 weKEA입니다.</div>;
 }
 
 export default Main;
