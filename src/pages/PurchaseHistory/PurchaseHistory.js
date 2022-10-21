b34117c70d6da27ac558f6dd01974135f2511118
김도영 <go3152@gmail.com>
2022-10-21 10:12:38 +0900

    ADD:구매리스트 페이지 레이아웃 중간저장

--- src/pages/PurchaseHistory/PurchaseHistory.js
+++ src/pages/PurchaseHistory/PurchaseHistory.js
@@ -4,17 +4,17 @@ import "./PurchaseHistory.scss";
 function PurchaseHistory() {
   return (
     <div className="purchase-history">
-      <div className="left">
-        <div className="content">
-          <div className="top">구매 내역</div>
-          <div className="bottom">
+      <div className="order-history">
+        <div className="order-history-content-box">
+          <div className="purchase-history-text">구매 내역</div>
+          <div className="question-about-omission">
             주문이 목록에서 누락 되었나요?{" "}
-            <span className="span">주문 조회</span> 기능으로 주문을 조회해
-            보세요.
+            <span className="link-to-order-list">주문 조회</span> 기능으로
+            주문을 조회해 보세요.
           </div>
         </div>
       </div>
-      <div className="right">dd</div>
+      <div className="purchase-list-box">dddd</div>
     </div>
   );
 }
