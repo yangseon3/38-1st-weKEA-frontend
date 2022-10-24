898d3cddb9735f1b4e51e128ae6e09691e84d2ca
김도영 <go3152@gmail.com>
2022-10-24 21:30:10 +0900

    modify : navigate함수 위치 이동

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -24,9 +24,11 @@ function Main() {
       <Nav />
       <div className="main-page">
         <div className="main-all-image-frame">
-          <div className="main-image-wrapper desk-image-wrapper">
+          <div
+            className="main-image-wrapper desk-image-wrapper"
+            onClick={goToDeskListPage}
+          >
             <img
-              onClick={goToDeskListPage}
               alt="desk"
               className="main-desk-img main-page-4image"
               src="https://secure.img1-fg.wfcdn.com/im/28077962/compr-r85/1765/176532736/default.jpg"
@@ -34,9 +36,11 @@ function Main() {
             <div className="category-text desk-categoty-text">Desk</div>
           </div>
 
-          <div className="main-image-wrapper chair-image-wrapper">
+          <div
+            className="main-image-wrapper chair-image-wrapper"
+            onClick={goToChairListPage}
+          >
             <img
-              onClick={goToChairListPage}
               alt="chair"
               className="main-chair-img main-page-4image"
               src="https://www.target.com.au/medias/static_content/product/images/full/85/83/A1788583.jpg?impolicy=product_portrait_hero"
@@ -44,9 +48,11 @@ function Main() {
             <div className="category-text chair-categoty-text">Chair</div>
           </div>
 
-          <div className="main-image-wrapper drawer-image-wrapper">
+          <div
+            className="main-image-wrapper drawer-image-wrapper"
+            onClick={goToDrawerListPage}
+          >
             <img
-              onClick={goToDrawerListPage}
               className="main-drawer-img main-page-4image"
               alt="drawer"
               src="https://assets.pbimgs.com/pbimgs/ab/images/dp/wcm/202224/0212/livingston-9-drawer-wide-dresser-1-c.jpg"
@@ -54,19 +60,17 @@ function Main() {
             <div className="category-text drawer-categoty-text">drawer</div>
           </div>
 
-          <div className="main-image-wrapper shelf-image-wrapper">
+          <div
+            className="main-image-wrapper shelf-image-wrapper"
+            onClick={goToShelfListPage}
+          >
             <img
-              onClick={goToShelfListPage}
               className="main-shelf-img main-page-4image"
               alt="shelf"
               src="https://www.ikea.com/kr/en/images/products/burhult-sibbhult-wall-shelf-combination-white-white__1092767_pe863011_s5.jpg?f=s"
             />
             <div className="category-text shelf-categoty-text">shelf</div>
           </div>
-        </div>
-
-        <div className="main-carousel-area">
-          캐러셀 들어갈 자리@@@@@@@@@@@@@
         </div>
 
         <div>위코드 38기 3팀 weKEA입니다.</div>
