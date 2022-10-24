6db539ed8edb92fd51b534940d01ce9d8b54179f
김도영 <go3152@gmail.com>
2022-10-24 21:14:26 +0900

    modify:주석 삭제

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import PurchaseHistory from "../PurchaseHistory/PurchaseHistory.js";
 function Main() {
-  return <div>위코드 38기 3팀 weKEA입니다.</div>;
+  return (
+    <div>
+      <PurchaseHistory />
+      위코드 38기 3팀 weKEA입니다.
+    </div>
+  );
 }
 
 export default Main;
