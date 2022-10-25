8909e630330a31d68f0f285c6b292887f0ee7c9e
이은오 <leo@LEO-MackBook.local>
2022-10-25 20:23:50 +0900

    Modal 수정

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,16 +2,10 @@ import React from "react";
 import "./Modal.scss";
 
 const Modal = ({ modalClose }) => {
-  // const onCloseModal = e => {
-  //   if (e.target === e.currentTarget) {
-  //     modalClose();
-  //   }
-  // };
   return (
     <div className="modal-container">
-      {/* onClick={onCloseModal}화면 어디를 클릭해도 모달창이 없어짐 */}
       <div className="modal-content">
-        {"가입하지 않은 아이디이거나, 잘못된 비밀번호입니다."}
+        가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.
         <button className="modal-button" onClick={modalClose}>
           X
         </button>
