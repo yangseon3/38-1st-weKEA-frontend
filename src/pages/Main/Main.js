d33e41e7102277a3ed6f43112cfe7bac77d00331
김도영 <go3152@gmail.com>
2022-10-26 21:56:15 +0900

    modify:메인 이미지 4개를 map함수로 렌더링

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,6 +5,53 @@ import "./Main.scss";
 
 function Main() {
   const navigate = useNavigate();
+  const goToListPage = categoty => {
+    navigate(`/category/${categoty}`);
+  };
+
+  const MAIN_PAGE_CATERORY_BOX_CONST_DATA = [
+    {
+      id: "1",
+      onclickfunction: goToListPage,
+      wrapperClassName: "desk-image-wrapper",
+      textClassName: "desk-categoty-text",
+      imgClassName: "main-desk-img",
+      imgSrc:
+        "https://secure.img1-fg.wfcdn.com/im/28077962/compr-r85/1765/176532736/default.jpg",
+      alt: "desk",
+    },
+    {
+      id: "2",
+      onclickfunction: goToListPage,
+      wrapperClassName: "chair-image-wrapper",
+      textClassName: "chair-categoty-text",
+      imgClassName: "main-chair-img",
+      imgSrc:
+        "https://www.target.com.au/medias/static_content/product/images/full/85/83/A1788583.jpg?impolicy=product_portrait_hero",
+      alt: "chair",
+    },
+
+    {
+      id: "3",
+      onclickfunction: goToListPage,
+      wrapperClassName: "drawer-image-wrapper",
+      textClassName: "drawer-categoty-text",
+      imgClassName: "main-drawer-img",
+      imgSrc:
+        "https://www.target.com.au/medias/static_content/product/images/full/85/83/A1788583.jpg?impolicy=product_portrait_hero",
+      alt: "drawer",
+    },
+    {
+      id: "4",
+      onclickfunction: goToListPage,
+      wrapperClassName: "shelf-image-wrapper",
+      textClassName: "shelf-categoty-text",
+      imgClassName: "main-shelf-img",
+      imgSrc:
+        "https://www.ikea.com/kr/en/images/products/burhult-sibbhult-wall-shelf-combination-white-white__1092767_pe863011_s5.jpg?f=s",
+      alt: "shelf",
+    },
+  ];
 
   const goToChairListPage = () => {
     navigate("/category/chair");
@@ -24,50 +71,26 @@ function Main() {
       <Nav />
       <div className="main-page">
         <div className="main-all-image-frame">
-          <div
-            className="main-image-wrapper desk-image-wrapper"
-            onClick={goToDeskListPage}
-          >
-            <img
-              alt="desk"
-              className="main-desk-img main-page-4image"
-              src="https://secure.img1-fg.wfcdn.com/im/28077962/compr-r85/1765/176532736/default.jpg"
-            />
-            <div className="category-text desk-categoty-text">Desk</div>
-          </div>
-
-          <div
-            className="main-image-wrapper chair-image-wrapper"
-            onClick={goToChairListPage}
-          >
-            <img
-              alt="chair"
-              className="main-page-4image"
-              src="https://www.target.com.au/medias/static_content/product/images/full/85/83/A1788583.jpg?impolicy=product_portrait_hero"
-            />
-            <div className="category-text chair-categoty-text">Chair</div>
-          </div>
-
-          <div className="main-image-wrapper " onClick={goToDrawerListPage}>
-            <img
-              className="main-page-4image"
-              alt="drawer"
-              src="https://assets.pbimgs.com/pbimgs/ab/images/dp/wcm/202224/0212/livingston-9-drawer-wide-dresser-1-c.jpg"
-            />
-            <div className="category-text drawer-categoty-text">drawer</div>
-          </div>
-
-          <div
-            className="main-image-wrapper shelf-image-wrapper"
-            onClick={goToShelfListPage}
-          >
-            <img
-              className="main-page-4image"
-              alt="shelf"
-              src="https://www.ikea.com/kr/en/images/products/burhult-sibbhult-wall-shelf-combination-white-white__1092767_pe863011_s5.jpg?f=s"
-            />
-            <div className="category-text shelf-categoty-text">shelf</div>
-          </div>
+          {MAIN_PAGE_CATERORY_BOX_CONST_DATA.map(categoryInfo => {
+            return (
+              <div
+                className="main-image-wrapper desk-image-wrapper"
+                onClick={() => {
+                  goToListPage(categoryInfo.alt);
+                }}
+                key={categoryInfo.id}
+              >
+                <img
+                  alt={categoryInfo.alt}
+                  className="main-desk-img main-page-4image"
+                  src={categoryInfo.imgSrc}
+                />
+                <div className="category-text desk-categoty-text">
+                  {categoryInfo.alt}
+                </div>
+              </div>
+            );
+          })}
         </div>
 
         <div>위코드 38기 3팀 weKEA입니다.</div>
