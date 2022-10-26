4bab6d9bc27df9b81cef35e0e9072eb6d28923f4
김도영 <go3152@gmail.com>
2022-10-26 18:30:38 +0900

    modify:메인페이지 원상복구

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Listpage from "../Listpage/Listpage";
+
 function Main() {
-  return (
-    <div>
-      <Listpage />
-      위코드 38기 3팀 weKEA입니다.
-    </div>
-  );
+  return <div>위코드 38기 3팀 weKEA입니다.</div>;
 }
 
 export default Main;
