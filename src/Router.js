fddeb894fe2b52604a1bb95c145a09e63b0f9cf3
김도영 <go3152@gmail.com>
2022-10-27 16:50:42 +0900

    master merge전 커밋

--- src/Router.js
+++ src/Router.js
@@ -4,12 +4,10 @@ import Main from "./pages/Main/Main";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import Cart from "./pages/Cart/Cart";
-import Desk from "./pages/Categories/Desk";
-import Chair from "./pages/Categories/Chair";
-import Drawer from "./pages/Categories/Drawer";
-import Shelf from "./pages/Categories/Shelf";
-import Mypage from "../src/pages/Mypage/Mypage";
 import Nav from "./components/Nav/Nav";
+import Mypage from "../src/pages/Mypage/Mypage";
+import WishList from "./pages/WishList/WishList";
+import Listpage from "./pages/Listpage/Listpage";
 
 function Router() {
   return (
@@ -20,11 +18,9 @@ function Router() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/category/desk" element={<Desk />} />
-        <Route path="/category/chair" element={<Chair />} />
-        <Route path="/category/drawer" element={<Drawer />} />
-        <Route path="/category/shelf" element={<Shelf />} />
+        <Route path="/category/:categoryId" element={<Listpage />} />
         <Route path="/mypage" element={<Mypage />} />
+        <Route path="/wishlist" element={<WishList />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,15 +5,17 @@ import "./Main.scss";
 
 function Main() {
   const navigate = useNavigate();
-  const goToListPage = categoty => {
-    navigate(`/category/${categoty}`);
+  const goToListPage = categoryId => {
+    navigate(`/category/${categoryId}`);
   };
-
-  const MAIN_PAGE_CATERORY_BOX_CONST_DATA = [
+  //책상 의자 서랍 선반 순서로 category
+  const MAIN_PAGE_CATEGORY_BOX_CONST_DATA = [
     {
       id: "1",
+      categoryId: "1",
+      category: "desk",
       wrapperClassName: "desk-image-wrapper",
-      textClassName: "desk-categoty-text",
+      textClassName: "desk-category-text",
       imgClassName: "main-desk-img",
       imgSrc:
         "https://secure.img1-fg.wfcdn.com/im/28077962/compr-r85/1765/176532736/default.jpg",
@@ -21,8 +23,10 @@ function Main() {
     },
     {
       id: "2",
+      categoryId: "2",
+      category: "chair",
       wrapperClassName: "chair-image-wrapper",
-      textClassName: "chair-categoty-text",
+      textClassName: "chair-category-text",
       imgClassName: "main-chair-img",
       imgSrc:
         "https://www.target.com.au/medias/static_content/product/images/full/85/83/A1788583.jpg?impolicy=product_portrait_hero",
@@ -31,8 +35,10 @@ function Main() {
 
     {
       id: "3",
+      categoryId: "3",
+      category: "drawer",
       wrapperClassName: "drawer-image-wrapper",
-      textClassName: "drawer-categoty-text",
+      textClassName: "drawer-category-text",
       imgClassName: "main-drawer-img",
       imgSrc:
         "https://www.target.com.au/medias/static_content/product/images/full/85/83/A1788583.jpg?impolicy=product_portrait_hero",
@@ -40,8 +46,10 @@ function Main() {
     },
     {
       id: "4",
+      categoryId: "4",
+      category: "shelf",
       wrapperClassName: "shelf-image-wrapper",
-      textClassName: "shelf-categoty-text",
+      textClassName: "shelf-category-text",
       imgClassName: "main-shelf-img",
       imgSrc:
         "https://www.ikea.com/kr/en/images/products/burhult-sibbhult-wall-shelf-combination-white-white__1092767_pe863011_s5.jpg?f=s",
@@ -54,12 +62,12 @@ function Main() {
       <Nav />
       <div className="main-page">
         <div className="main-all-image-frame">
-          {MAIN_PAGE_CATERORY_BOX_CONST_DATA.map(categoryInfo => {
+          {MAIN_PAGE_CATEGORY_BOX_CONST_DATA.map(categoryInfo => {
             return (
               <div
                 className="main-image-wrapper desk-image-wrapper"
                 onClick={() => {
-                  goToListPage(categoryInfo.alt);
+                  goToListPage(categoryInfo.categoryId);
                 }}
                 key={categoryInfo.id}
               >
@@ -68,8 +76,8 @@ function Main() {
                   className="main-desk-img main-page-4image"
                   src={categoryInfo.imgSrc}
                 />
-                <div className="category-text desk-categoty-text">
-                  {categoryInfo.alt}
+                <div className="category-text desk-category-text">
+                  {categoryInfo.category}
                 </div>
               </div>
             );
