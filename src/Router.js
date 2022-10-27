b1f3ddcd3703bee07c81fa49e7824a55fb694a48
김도영 <go3152@gmail.com>
2022-10-27 18:27:18 +0900

    컨플릭트 해결중 테스트

--- src/Router.js
+++ src/Router.js
@@ -8,6 +8,7 @@ import Nav from "./components/Nav/Nav";
 import Mypage from "../src/pages/Mypage/Mypage";
 import WishList from "./pages/WishList/WishList";
 import Listpage from "./pages/Listpage/Listpage";
+import PurchaseHistory from "./pages/PurchaseHistory/PurchaseHistory";
 
 function Router() {
   return (
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Main.scss";
 
 function Main() {
-  return <div>위코드 38기 3팀 weKEA입니다.</div>;
+  return;
 }
 
 export default Main;
